7112bccc2013544983362d42d793572c0b3a61ee
Nicholas Ng <nicholasykng@gmail.com>
2019-10-26 22:30:39 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -34,9 +34,10 @@ def self.find_by_name(name)
 end
 
 def self.find_or_create_by_name(name)
-  self.all.include?(name)
+  if self.all.include?(name)
     return self.find_by_name(name)
-  else self.create_by_name(name)
+  else 
+    return self.create_by_name(name)
 end
 end
 end
