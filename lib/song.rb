e8f89acbae17893923ffd0fc1f9e8b77dd6e8882
Nicholas Ng <nicholasykng@gmail.com>
2019-10-26 22:34:52 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -35,9 +35,9 @@ end
 
 def self.find_or_create_by_name(name)
   if @@all.find {|song| song.name == name}
-    return self.find_by_name
+    return self.find_by_name(name)
   else
-    return self.create_by_name
+    return self.create_by_name(name)
 end
 end
 end
