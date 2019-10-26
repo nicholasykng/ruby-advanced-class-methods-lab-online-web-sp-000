63ad7b3f365a23afb04b14ee695a41dc397fd081
Nicholas Ng <nicholasykng@gmail.com>
2019-10-26 22:01:29 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -9,11 +9,13 @@ class Song
   def save
     self.class.all << self
   end
+
   def self.create
     song = self.new
     @@all << song
     return song
   end
+
 def self.new_by_name(name)
   song = self.new
   song.name = name
